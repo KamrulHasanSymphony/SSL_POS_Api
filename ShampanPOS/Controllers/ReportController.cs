8700a60cb5bcf000a11ffdea1890d781c25eb4c1
DEV-1020\Rukaiya <rukaiya.akhter@symphonysoftt.com>
2026-01-28 10:18:39 +0600

    Add SaleOrderReport

--- ShampanPOS/Controllers/ReportController.cs
+++ ShampanPOS/Controllers/ReportController.cs
@@ -137,6 +137,36 @@ namespace ShampanPOS.Controllers
             }
         }
 
+        [HttpPost("GetSaleOrderReport")]
+        public async Task<ResultVM> GetSaleOrderReport(CommonVM Vm)
+        {
+            ResultVM resultVM = new ResultVM { Status = "Fail", Message = "Error", ExMessage = null, Id = "0", DataVM = null };
+            try
+            {
+                string[] conditionFields = null;
+                string[] conditionValues = null;
+
+                if (!string.IsNullOrEmpty(Vm.Id))
+                {
+                    conditionFields = new string[] { "M.Id" };
+                    conditionValues = new string[] { Vm.Id };
+                }
+
+                SaleOrderService _service = new SaleOrderService();
+                resultVM = await _service.List(conditionFields, conditionValues, null);
+                return resultVM;
+            }
+            catch (Exception ex)
+            {
+                return new ResultVM
+                {
+                    Status = "Fail",
+                    Message = "Data not fetched.",
+                    ExMessage = ex.Message,
+                    DataVM = null
+                };
+            }
+        }
 
     }
 }
